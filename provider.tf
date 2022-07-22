6656e9553671a35586a397a6c027c8d49609fa57
pravpr <109223637+pravpr@users.noreply.github.com>
2022-07-22 16:32:13 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,5 @@
-provider "aws" {}
+provider "aws" {
+  region     = "us-east-2"
+  shared_config_files      = ["%USERPROFILE%\\.aws\\config"]
+  shared_credentials_files = ["%USERPROFILE%\\.aws\\credentials"]
+}
