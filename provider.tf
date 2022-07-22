b3330732a26db7acadf951ee4ac82ba670785db0
pravpr <109223637+pravpr@users.noreply.github.com>
2022-07-22 20:55:17 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,11 @@
+# If you are using windows OS then use the below
+# If you are using Linux uncomment the commented lines and comment the previous 2 lines
 provider "aws" {
   region     = "us-east-2"
+  shared_config_files      = ["$HOME/.aws/config"]
+  shared_credentials_files = ["$HOME/.aws/credentials"]
+  /*
   shared_config_files      = ["%USERPROFILE%\\.aws\\config"]
   shared_credentials_files = ["%USERPROFILE%\\.aws\\credentials"]
+  */
 }
